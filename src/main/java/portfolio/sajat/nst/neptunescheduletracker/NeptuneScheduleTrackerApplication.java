fe089b8538e652d6b488a9c5090bfa2577e8b407
Zsófia Győri <zsofia.gyori00@gmail.com>
2024-03-08 13:43:48 +0100

    init

---
+++ src/main/java/portfolio/sajat/nst/neptunescheduletracker/NeptuneScheduleTrackerApplication.java
@@ -0,0 +1,13 @@
+package portfolio.sajat.nst.neptunescheduletracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeptuneScheduleTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeptuneScheduleTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/portfolio/sajat/nst/neptunescheduletracker/NeptuneScheduleTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.sajat.nst.neptunescheduletracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeptuneScheduleTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
