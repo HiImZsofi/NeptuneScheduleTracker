fec41a5b30f8f9193e6e753e0931d2c6f43d91ed
ZsofiaGyori <zsofia.gyori00@gmail.com>
2024-03-12 15:17:37 +0100

    Replace boilerplate with lombok annotations

--- src/main/java/portfolio/sajat/nst/neptunescheduletracker/entities/User.java
+++ src/main/java/portfolio/sajat/nst/neptunescheduletracker/entities/User.java
@@ -1,6 +1,9 @@
 package portfolio.sajat.nst.neptunescheduletracker.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
