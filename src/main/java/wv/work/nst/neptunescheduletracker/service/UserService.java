af68ea80394364a426aacf8e3af8141e25b2f599
ZsofiaGyori <zsofia.gyori00@gmail.com>
2024-04-09 16:47:25 +0200

    User service

--- src/main/java/wv/work/nst/neptunescheduletracker/service/UserService.java
+++ src/main/java/wv/work/nst/neptunescheduletracker/service/UserService.java
@@ -1,4 +1,17 @@
 package wv.work.nst.neptunescheduletracker.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import wv.work.nst.neptunescheduletracker.entity.User;
+import wv.work.nst.neptunescheduletracker.repository.UserRepository;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    //TODO password encoder
+    public User createUser(User user, String password) {
+        return userRepository.save(user);
+    }
 }
