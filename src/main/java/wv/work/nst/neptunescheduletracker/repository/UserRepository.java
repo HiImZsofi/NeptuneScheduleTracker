a7d66f54a9f95843da8f3b512fb291c68609ed3c
ZsofiaGyori <zsofia.gyori00@gmail.com>
2024-04-09 16:42:42 +0200

    UserRepository interface

--- src/main/java/wv/work/nst/neptunescheduletracker/repository/UserRepository.java
+++ src/main/java/wv/work/nst/neptunescheduletracker/repository/UserRepository.java
@@ -1,9 +1,10 @@
-package wv.work.nst.neptunescheduletracker.repositories;
+package wv.work.nst.neptunescheduletracker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import wv.work.nst.neptunescheduletracker.entities.User;
+import wv.work.nst.neptunescheduletracker.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findOneByEmail(String email);
 }
