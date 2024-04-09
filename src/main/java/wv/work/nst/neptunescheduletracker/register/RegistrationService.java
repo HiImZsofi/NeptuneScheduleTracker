05fb2076414dced5dce94cedf4e2902170df6d29
ZsofiaGyori <zsofia.gyori00@gmail.com>
2024-04-09 16:42:09 +0200

    Reg service

--- src/main/java/wv/work/nst/neptunescheduletracker/register/RegistrationService.java
+++ src/main/java/wv/work/nst/neptunescheduletracker/register/RegistrationService.java
@@ -1,4 +1,22 @@
 package wv.work.nst.neptunescheduletracker.register;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import wv.work.nst.neptunescheduletracker.entity.User;
+import wv.work.nst.neptunescheduletracker.service.UserService;
+
+@Service
 public class RegistrationService {
+
+    @Autowired
+    UserService userService;
+
+    public User register(RegistrationInfo registrationInfo){
+        User user = new User();
+        user.setFirstName(registrationInfo.getFirstName());
+        user.setLastName(registrationInfo.getLastName());
+        user.setEmail(registrationInfo.getEmail()); //TODO neptune kod meg a tobbi
+
+        return userService.createUser(user, registrationInfo.getPassword());
+    }
 }
