7660e0fe095383e043981fae527c8d4495785370
ZsofiaGyori <zsofia.gyori00@gmail.com>
2024-04-11 08:42:50 +0200

    Add password field to entity

--- src/main/java/wv/work/nst/neptunescheduletracker/entity/User.java
+++ src/main/java/wv/work/nst/neptunescheduletracker/entity/User.java
@@ -3,6 +3,7 @@ package wv.work.nst.neptunescheduletracker.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
+@AllArgsConstructor
 @Setter
 @Getter
 public class User extends BaseEntity{
@@ -31,6 +33,8 @@ public class User extends BaseEntity{
     private int currentSemester;
     @Column
     private int subjectId;
+    @Column
+    private String password;
 
     public User(String firstName,String lastName, String neptuneCode, LocalDate dateOfBirth, String startOfStudies, String email){
         super();
@@ -40,5 +44,13 @@ public class User extends BaseEntity{
         this.startOfStudies = startOfStudies;
         this.neptuneCode = neptuneCode;
         this.email = email;
+    }
+
+    public User(String firstName, String lastName, String email, String password){
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
     }
 }
