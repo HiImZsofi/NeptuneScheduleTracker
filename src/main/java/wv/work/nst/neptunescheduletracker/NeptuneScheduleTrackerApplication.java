9e26aa441b4f25f1b5138a220fab4b0cddb681af
ZsofiaGyori <zsofia.gyori00@gmail.com>
2024-04-16 16:39:20 +0200

    delete everything from main method

--- src/main/java/wv/work/nst/neptunescheduletracker/NeptuneScheduleTrackerApplication.java
+++ src/main/java/wv/work/nst/neptunescheduletracker/NeptuneScheduleTrackerApplication.java
@@ -2,12 +2,13 @@ package wv.work.nst.neptunescheduletracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class NeptuneScheduleTrackerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NeptuneScheduleTrackerApplication.class, args);
+        ApplicationContext context = SpringApplication.run(NeptuneScheduleTrackerApplication.class, args);
     }
 
 }
