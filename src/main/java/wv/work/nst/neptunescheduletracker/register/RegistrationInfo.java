b1664c519be1df4f524ffcfa78b5a522baae3848
ZsofiaGyori <zsofia.gyori00@gmail.com>
2024-04-18 10:17:42 +0200

    move validation from main data class to a separate one

--- src/main/java/wv/work/nst/neptunescheduletracker/register/RegistrationInfo.java
+++ src/main/java/wv/work/nst/neptunescheduletracker/register/RegistrationInfo.java
@@ -1,10 +1,13 @@
 package wv.work.nst.neptunescheduletracker.register;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import wv.work.nst.neptunescheduletracker.repository.UserRepository;
 import wv.work.nst.neptunescheduletracker.security.validate.InfoValidator;
@@ -14,7 +17,7 @@ import wv.work.nst.neptunescheduletracker.security.validate.Validateable;
  Data class for information required for registration
 */
 @Data
-public class RegistrationInfo implements Validateable<RegistrationInfo> {
+public class RegistrationInfo{
 
     @NotBlank(message = "A vezetéknév nem lehet üres!")
     private String firstName;
@@ -25,22 +28,4 @@ public class RegistrationInfo implements Validateable<RegistrationInfo> {
     private String email;
     @Size(min = 6, max = 30, message = "A jelszónak legalább 6 karakter hosszúnak kell lennie!")
     private String password;
-
-
-    @Override
-    public InfoValidator<RegistrationInfo> validator() {
-        return new InfoValidator<RegistrationInfo>() {
-
-            @Autowired
-            UserRepository userRepository;
-
-            @Override
-            public void validate(RegistrationInfo target, Errors errors) {
-
-                if (userRepository.findOneByEmail(target.getEmail()) != null) {
-                    errors.rejectValue("email", "", "Ez az email cím már foglalt!");
-                }
-            }
-        };
-    }
 }
--- src/main/java/wv/work/nst/neptunescheduletracker/security/validate/ValidateRegistry.java
+++ src/main/java/wv/work/nst/neptunescheduletracker/security/validate/ValidateRegistry.java
@@ -1,4 +1,32 @@
 package wv.work.nst.neptunescheduletracker.security.validate;
 
-public class ValidateRegistry {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import wv.work.nst.neptunescheduletracker.register.RegistrationInfo;
+import wv.work.nst.neptunescheduletracker.repository.UserRepository;
+
+@Component
+public class ValidateRegistry implements Validateable<RegistrationInfo>{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public ValidateRegistry(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public InfoValidator<RegistrationInfo> validator() {
+        return new InfoValidator<RegistrationInfo>() {
+
+            @Override
+            public void validate(RegistrationInfo target, Errors errors) {
+
+                if (userRepository.findOneByEmail(target.getEmail()) != null) {
+                    errors.rejectValue("email", "", "Ez az email cím már foglalt!");
+                }
+            }
+        };
+    }
 }
