5f169e8c730dfeb89d5f301ba9c01aa5eb907dda
ZsofiaGyori <zsofia.gyori00@gmail.com>
2024-05-22 11:55:30 +0200

    revert changes

--- neptune_schedule_tracker_frontend/src/index.js
+++ neptune_schedule_tracker_frontend/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Registration from "./registration/Registration";
+import Login from "./login/Login";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Registration />
+    <App />
   </React.StrictMode>
 );
 
